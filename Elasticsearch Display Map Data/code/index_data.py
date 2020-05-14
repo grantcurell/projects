b05a76f3a05615b7f618d0557f01bb3bd9121729
Grant Curell <grantcurell@gmail.com>
2020-05-14 13:07:37 -0500

    Fix minor bugs

--- Elasticsearch Display Map Data/code/index_data.py
+++ Elasticsearch Display Map Data/code/index_data.py
@@ -7,20 +7,19 @@ def geojson_to_es(gj):
 
     for feature in gj['features']:
 
-        date = datetime.strptime("-".join(feature["properties"]["event_date"].split('-')[0:2]) + "-" + feature["properties"]["year"], "%d-%B-%Y")
+        date = datetime.strptime("-".join(feature["properties"]["event_date"].split('-')[0:2]) + "-" + feature["properties"]["year"], "%d-%b-%Y")
         feature["properties"]["timestamp"] = int(date.timestamp())
         feature["properties"]["event_date"] = date.strftime('%Y-%m-%d')
         yield feature
 
 
-with open("test_point.json") as f:
+with open("GeoObs.json") as f:
     gj = geojson.load(f)
 
     es = Elasticsearch(hosts=[{'host': 'localhost', 'port': 9200}])
 
     k = ({
         "_index": "conflict-data",
-        "_type": "_doc",
         "_source": feature,
     } for feature in geojson_to_es(gj))
 
