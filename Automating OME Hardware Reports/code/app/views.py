4aa3882afeb0ab55f19c542ff94f5b5cbdc17c45
Grant Curell <grantcurell@gmail.com>
2020-07-31 11:05:21 -0400

    Health check working

--- Automating OME Hardware Reports/code/app/views.py
+++ Automating OME Hardware Reports/code/app/views.py
@@ -21,72 +21,171 @@ from flask_cors import CORS
 from app import app
 from lib.discover_device import discover_device, get_job_id, track_job_to_completion
 from lib.ome import authenticate_with_ome, get_ips
-from flask_restful import Resource, Api
 import json
 import logging
 import time
+import requests
+from urllib3 import disable_warnings
 
 CORS(app)
-api = Api(app)
+
+servers = {'192.168.1.10': 12446}
+
+# This just stops urllib3 from complaining endlessly that we're making insecure connections. I know. I didn't set it up
+# to do proper encryption. Sue me.
+disable_warnings()
+
+def _validate_ome_and_target(json_data: dict) -> bool:
+
+    if "target_ips" not in json_data:
+        logging.error("JSON data is missing the field \'target_ips\'")
+        return False
+    elif "ome_ip_address" not in json_data:
+        logging.error("JSON data is missing the field \'ome_ip_address\'")
+        return False
+    elif "user_name" not in json_data:
+        logging.error("JSON data is missing the field \'user_name\'")
+        return False
+    elif "password" not in json_data:
+        logging.error("JSON data is missing the field \'password\'")
+        return False
+
+    return True
 
 
-class Discover(Resource):
+def get_device_id_by_ip(ome_ip_address: str, target_ip_address: str, headers: dict) -> str:
 
-    def put(self, target_ips: list, ome_ip_address: str, user_name: str, password: str, discover_user_name: str,
-            discover_password: str, device_type: str):
-        """
-        json_data = request.get_json()
+    url = "https://%s/api/DeviceService/Devices?$filter=DeviceName eq \'%s\'" % (ome_ip_address, target_ip_address)
 
-        if "target_ips" not in json_data:
-            logging.error("JSON data is missing the field \'target_ips\'")
-            return app.response_class(status=400)
-        elif "ome_ip_address" not in json_data:
-            logging.error("JSON data is missing the field \'ome_ip_address\'")
-            return app.response_class(status=400)
-        elif "user_name" not in json_data:
-            logging.error("JSON data is missing the field \'user_name\'")
-            return app.response_class(status=400)
-        elif "password" not in json_data:
-            logging.error("JSON data is missing the field \'password\'")
-            return app.response_class(status=400)
-        elif "discover_user_name" not in json_data:
-            logging.error("JSON data is missing the field \'discover_user_name\'")
-            return app.response_class(status=400)
-        elif "discover_password" not in json_data:
-            logging.error("JSON data is missing the field \'discover_password\'")
-            return app.response_class(status=400)
-        elif "device_type" not in json_data:
-            logging.error("JSON data is missing the field \'device_type\'")
-            return app.response_class(status=400)
+    response = requests.get(url, headers=headers, verify=False)
 
-        target_ips = get_ips(json_data["target_ips"])
-        ome_ip_address = json_data["ome_ip_address"]
-        user_name = json_data["user_name"]
-        password = json_data["password"]
-        discover_user_name = json_data["discover_user_name"]
-        discover_password = json_data["discover_password"]
-        device_type = json_data["device_type"]
-        """
+    if response.status_code == 200:
+        json_data = response.json()
+        if json_data['@odata.count'] == 1:
+            return json_data['value'][0]['Id']
+        elif json_data['@odata.count'] > 1:
+            logging.warning("WARNING: We found more than one name that matched " + target_ip_address + ". Returning "
+                                                                                                       "the first.")
+            return json_data['value'][0]['Id']
+        else:
+            logging.error("Not results returned for device ID look up for name " + target_ip_address)
+            return ""
+    else:
+        return ""
 
-        try:
-            auth_success, headers = authenticate_with_ome(ome_ip_address, user_name,
-                                                          password)
-            if auth_success:
-                discover_resp = discover_device(ome_ip_address, headers,
-                                                discover_user_name, discover_password,
-                                                target_ips, device_type)
-                if discover_resp.status_code == 201:
-                    logging.info("Discovering devices.....")
-                    time.sleep(30)
-                    discovery_config_group_id = (discover_resp.json())["DiscoveryConfigGroupId"]
-                    job_id = get_job_id(ome_ip_address, headers, discovery_config_group_id)
-                    if job_id != -1:
-                        track_job_to_completion(ome_ip_address, headers, job_id)
-                else:
-                    logging.error("unable to discover devices ", discover_resp.text)
-                    return app.response_class(status=404)
-        except Exception as e:
-            logging.error("Unexpected error:", str(e))
-            return app.response_class(status=500)
 
-        return app.response_class(status=200)
+@app.route('/api/discover', methods=['PUT'])
+def discover():
+    """
+    Discovers a list of devices based on input
+
+    Example curl:
+    
+    curl -XPUT -d '{"target_ips": "192.168.1.10", "ome_ip_address": "192.168.1.18", "user_name": "admin", 
+    "password": "password", "discover_user_name": "root", "discover_password": "password", 
+    "device_type": "server"}' 127.0.0.1:5000/api/discover -H "Content-Type: application/json"
+    """
+
+    json_data = request.get_json()
+
+    if not _validate_ome_and_target(json_data):
+        return app.response_class(status=400)
+    elif "discover_user_name" not in json_data:
+        logging.error("JSON data is missing the field \'discover_user_name\'")
+        return app.response_class(status=400)
+    elif "discover_password" not in json_data:
+        logging.error("JSON data is missing the field \'discover_password\'")
+        return app.response_class(status=400)
+    elif "device_type" not in json_data:
+        logging.error("JSON data is missing the field \'device_type\'")
+        return app.response_class(status=400)
+
+    target_ips = get_ips(json_data["target_ips"])
+    ome_ip_address = json_data["ome_ip_address"]
+    user_name = json_data["user_name"]
+    password = json_data["password"]
+    discover_user_name = json_data["discover_user_name"]
+    discover_password = json_data["discover_password"]
+    device_type = json_data["device_type"]
+
+    try:
+        auth_success, headers = authenticate_with_ome(ome_ip_address, user_name, password)
+        if auth_success:
+            discover_resp = discover_device(ome_ip_address, headers,
+                                            discover_user_name, discover_password,
+                                            target_ips, device_type)
+            if discover_resp.status_code == 201:
+                logging.info("Discovering devices.....")
+                time.sleep(30)
+                discovery_config_group_id = (discover_resp.json())["DiscoveryConfigGroupId"]
+                job_id = get_job_id(ome_ip_address, headers, discovery_config_group_id)
+                if job_id != -1:
+                    track_job_to_completion(ome_ip_address, headers, job_id)
+            else:
+                logging.error("unable to discover devices ", discover_resp.text)
+                return app.response_class(status=404)
+        else:
+            logging.warning("Failed to create a session to OpenManage. Are you sure your username and password for OME"
+                            " are correct?")
+            return app.response_class(status=401)
+    except Exception as e:
+        logging.error("Unexpected error:", str(e))
+        return app.response_class(status=500)
+
+    for ip in target_ips:
+        server_id = get_device_id_by_ip(ome_ip_address, ip, headers)
+        if server_id:
+            servers[ip] = server_id
+        else:
+            logging.warning("Error: couldn't resolve the name " + ip + " to a device ID.")
+            return app.response_class(status=404)
+
+    return app.response_class(status=200)
+
+
+@app.route('/api/hardware_health', methods=['GET'])
+def hardware_health():
+
+    json_data = request.get_json()
+
+    if not _validate_ome_and_target(json_data):
+        return app.response_class(status=400)
+
+    target_ips = get_ips(json_data["target_ips"])
+    ome_ip_address = json_data["ome_ip_address"]
+    user_name = json_data["user_name"]
+    password = json_data["password"]
+
+    auth_success, headers = authenticate_with_ome(ome_ip_address, user_name, password)
+    server_health = {}
+
+    for ip in target_ips:
+        logging.info("Getting the health for " + ip)
+        url = 'https://%s/api/DeviceService/Devices(%s)/SubSystemHealth' % (ome_ip_address, servers[ip])
+        response = requests.get(url, headers=headers, verify=False)
+        if response.status_code == 501:
+            logging.error("Failed to retrieve health. Error received " + str(response.content))
+            return app.response_class(status=501)
+        else:
+            server_health[ip] = json.loads(response.content)
+        logging.info("Retrieved health for " + ip)
+
+    return json.dumps(server_health), 201, {'Content-Type': 'application/json'}
+
+
+@app.route('/api/hardware_inventory', methods=['GET'])
+def hardware_inventory():
+
+    json_data = request.get_json()
+
+    if not _validate_ome_and_target(json_data):
+        return app.response_class(status=400)
+
+    target_ips = get_ips(json_data["target_ips"])
+    ome_ip_address = json_data["ome_ip_address"]
+    user_name = json_data["user_name"]
+    password = json_data["password"]
+
+    auth_success, headers = authenticate_with_ome(ome_ip_address, user_name, password)
+    server_health = {}
+
